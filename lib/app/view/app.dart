abcbe5a3f5314c66be561c97e69e7d4818e76cc1
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-05-24 02:55:58 -0600

    feat: Add core repository package to project
    This package has the task to setup and initialize all the core systems that the rest of packages and the project requires to function

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,12 +1,17 @@
+import 'package:core_repository/core_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:little_flutter_assessment/counter/counter.dart';
 import 'package:little_flutter_assessment/l10n/l10n.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Initialize the CoreRepository
+    ref.watch(coreRepositoryProvider).init();
+
     return MaterialApp(
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
